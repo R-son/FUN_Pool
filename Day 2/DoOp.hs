9bc73babed0a4ed3ffb10303f930374a543882b6
R-son <rsontheway@fedora>
2023-10-17 17:11:05 +0200

    [ADD] Day 2 Task 4

--- Day 2/DoOp.hs
+++ Day 2/DoOp.hs
@@ -1,5 +1,18 @@
+myElem :: Eq a => a -> [a] -> Bool
+myElem x [] = False
+myElem x (y:ys)
+    | x == y = True
+    | otherwise = myElem x ys
+
 safeDiv :: Int -> Int -> Maybe Int
 safeDiv _ 0 = Nothing
 safeDiv x y = Just (div x y)
 
 safeNth :: [a] -> Int -> Maybe a
+safeNth [] _ = Nothing
+safeNth (x:xs) 0 = Just x
+safeNth (x:xs) i = safeNth xs (i-1)
+
+safeSucc :: Maybe Int -> Maybe Int
+safeSucc Nothing = Nothing
+safeSucc (Just x) = Just (x + 1)
